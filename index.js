aa6448c51944700405e9ab25c982b15b81ac9a6a
Hicham MALKI <hicham.malki@lemonlearning.fr>
2023-02-11 19:43:29 +0100

    prepare to cooperate

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require("express");
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 
 var app = express();
+app.use(connectLiveReload());
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("Hello World! ... pp j a3");
 });
 
 app.listen(3000, function () {
