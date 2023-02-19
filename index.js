1eea5bb67176dfe235312dccfecb11170a8e6759
Hicham MALKI <hicham.malki@lemonlearning.fr>
2023-02-19 20:52:56 +0100

    docker run reload

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 var express = require("express");
-var livereload = require("livereload");
-var connectLiveReload = require("connect-livereload");
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
-
 
 var app = express();
-app.use(connectLiveReload());
-
-process.on('SIGINT', function() {
-  db.stop(function(err) {
-    process.exit(err ? 1 : 0);
-  });
-});
-
 
 app.get("/", function (req, res) {
-  res.send("Hello World! 3");
+  res.send("Hello World! 7");
 });
 
 app.listen(3000, function () {
