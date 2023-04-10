5482283daf07c48daa42fd4614af576b452898a7
root <root@dsp-archiwebf22-hm-hm-at.fr-2>
2023-04-10 15:16:07 +0000

    add tests + some checking + typo

---
+++ __tests__/admin.test.js
@@ -0,0 +1,16 @@
+
+require('dotenv').config();
+
+test('GET /api/admin/admin@thetiptop.fr returns object with role=admin', async () => {
+  const response = await fetch(`${process.env.HOST}:${process.env.PORT}/api/admin/admin@thetiptop.fr`);
+  const json = await response.json();
+  expect(response.status).toBe(201);
+  expect(json[0]).toMatchObject({ role: 'admin' });
+});
+
+test('GET /api/admin/employe@thetiptop.fr returns object with role=employe', async () => {
+  const response = await fetch(`${process.env.HOST}:${process.env.PORT}/api/admin/employe@thetiptop.fr`);
+  const json = await response.json();
+  expect(response.status).toBe(201);
+  expect(json[0]).toMatchObject({ role: 'employe' });
+});
