cd837be4680c11304ad4220e146ecc0180c3826b
Hicham MALKI <hicham.malki@lemonlearning.fr>
2023-04-11 12:08:59 +0200

    fix fetch problem

--- __tests__/admin.test.js
+++ __tests__/admin.test.js
@@ -2,14 +2,14 @@
 require('dotenv').config();
 
 test('GET /api/admin/admin@thetiptop.fr returns object with role=admin', async () => {
-  const response = await fetch(`${process.env.HOST}/api/admin/admin@thetiptop.fr`);
+  const response = await fetch(`https://dsp-archiwebf22-hm-hm-at.fr/api/admin/admin@thetiptop.fr`);
   const json = await response.json();
   expect(response.status).toBe(201);
   expect(json[0]).toMatchObject({ role: 'admin' });
 });
 
 test('GET /api/admin/employe@thetiptop.fr returns object with role=employe', async () => {
-  const response = await fetch(`${process.env.HOST}/api/admin/employe@thetiptop.fr`);
+  const response = await fetch(`https://dsp-archiwebf22-hm-hm-at.fr/api/admin/employe@thetiptop.fr`);
   const json = await response.json();
   expect(response.status).toBe(201);
   expect(json[0]).toMatchObject({ role: 'employe' });
